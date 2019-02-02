664a6c35efcba6a189536067b8a05a7dd9788692
Chris8018 <tranuytrieuvi@gmail.com>
2019-02-02 21:35:29 +0000

    Fix change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,12 @@
-package
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
